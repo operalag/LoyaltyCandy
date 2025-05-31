31d634c3851a865dc185dd02fc1c6d3beaa5f415
Tashi <tashi@example.com>
2025-05-31 15:20:28 +0600

    fix token ui update

--- Assets/SweetSugar/Scripts/ICP/NNS_II/ICPGetToken.cs
+++ Assets/SweetSugar/Scripts/ICP/NNS_II/ICPGetToken.cs
@@ -8,36 +8,16 @@ public class ICPGetToken : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI gemText;
     [SerializeField] private NNS_II nNS_II;
-    // [SerializeField] bool timelyCheck; // implement this one if timely check is needed
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(InspectChecking());
-        // StartCoroutine(TimerCheck(4));
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    // public IEnumerator TimerCheck(float seconds) // implement this one if timely check is needed 
-    // {
-    //     while (timelyCheck)
-    //     {
-    //         Task task = nNS_II.GetICPBalance();
-    //         while (!task.IsCompleted) yield return null;
-
-    //         InspectICPBalance();
-    //         yield return new WaitForSeconds(seconds);
-    //     }
-    // }
-
-    private IEnumerator InspectChecking()
-    {
-        Task task = nNS_II.GetICPBalance();
-        while (!task.IsCompleted) yield return null;
         InspectICPBalance();
     }
 
